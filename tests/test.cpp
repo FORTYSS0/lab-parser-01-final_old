d70ec1d4ef181f237c2bfb22f041b97ab3de75ae
FORTYSS <dmitriy_sviridov_2001@mail.ru>
2020-12-27 16:58:30 +0300

    Всё c тестами c разбивкой на файл36

--- tests/test.cpp
+++ tests/test.cpp
@@ -44,9 +44,9 @@ R"({
   std :: ofstream students;
   students.open(File, std::ios::out);
   students << string_t;
+  students.close();
   std::vector<Student> student = parser(File, len);
   bool flag = input(File);
-  students.close();
   EXPECT_TRUE(flag);
 }
 TEST(WFile, Emptynis) {
