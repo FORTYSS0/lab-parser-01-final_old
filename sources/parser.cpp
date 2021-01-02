8d057010b12ff63e0e2054aafbcc35f1535e4af1
FORTYSS <dmitriy_sviridov_2001@mail.ru>
2021-01-02 03:56:57 +0300

    Всё c тестами c разбивкой на файл48

--- sources/parser.cpp
+++ sources/parser.cpp
@@ -92,10 +92,10 @@ std::vector<Student> parser(const string& File, size_t len[4], json& data) {
       student_now.Name = student.at("name");
       if (static_cast<size_t>(student_now.Name.length()) > len[0])
         len[0] = static_cast<size_t>(student_now.Name.length());
+      std::cout << "as" << student_now.Name;
       student_now.Group = getValue(student, "group", len[1]);
       student_now.Avg = getValue(student, "avg", len[2]);
       student_now.Debt = getValue(student, "debt", len[3]);
-      std::cout << "as" << any_print(student_now.Debt);
       students.push_back(student_now);
     }
   }
