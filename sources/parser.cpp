b73933206c89ad2f1d3b21c0469c19531b6c9ed4
FORTYSS <dmitriy_sviridov_2001@mail.ru>
2021-01-02 05:34:43 +0300

    Всё c тестами c разбивкой на файл48

--- sources/parser.cpp
+++ sources/parser.cpp
@@ -58,9 +58,9 @@ size_t Size(const json& data, const string& valueName, size_t& stringLength) {
 std::any getValue(const json& data, const string& valueName,
                   size_t& stringLength) {
   //std::cout << "as" << data.at(valueName);
-  if (!data.at(valueName)) {
-    throw std::runtime_error{"There is no field with with name: " + valueName};
-  }
+  //if (!data.at(valueName)) {
+  //  throw std::runtime_error{"There is no field with with name: " + valueName};
+  //}
   if (data.at(valueName).is_number_integer()) {
     std::cout << "as" << data.at(valueName);
     stringLength = Size(data, valueName, stringLength);
