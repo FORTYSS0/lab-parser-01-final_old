eba9608efefa21fb87e4a2f91f5adcb3088b0d0b
FORTYSS <dmitriy_sviridov_2001@mail.ru>
2021-01-03 03:34:42 +0300

    Всё c тестами c разбивкой на файл48

--- include/student.h
+++ include/student.h
@@ -1,6 +1,8 @@
 #ifndef TEMPLATE_STUDENT_H
 #define TEMPLATE_STUDENT_H
 #include "parser.hpp"
+using any = std::any;
+using string = std::string;
 
 struct Student {
   string Name;
--- sources/parser.cpp
+++ sources/parser.cpp
@@ -71,10 +71,9 @@ size_t Size(const json& data, const string& valueName, size_t& stringLength) {
 }
 std::any getValue(const json& data, const string& valueName,
                   size_t& stringLength) {
-   //std::cout << " " << data.at(valueName) << " ";
-  //if (!data.at(valueName).empty()) {
-  //throw std::runtime_error{"There is no field with with name: " + valueName};
-  //} else {
+  if (!data.at(valueName).empty()) {
+  throw std::runtime_error{"There is no field with with name: " + valueName};
+  } else {
     if (data.at(valueName).is_number_integer()) {
       stringLength = Size(data, valueName, stringLength);
       //std::cout << "rrrrrrrr " << static_cast<int>(data.at(valueName)) << " ";
