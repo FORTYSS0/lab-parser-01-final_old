56f89775f1f401b6c3886974badbbacb13515428
FORTYSS <dmitriy_sviridov_2001@mail.ru>
2021-01-03 04:02:03 +0300

    Всё c тестами c разбивкой на файл48

--- include/parser.hpp
+++ include/parser.hpp
@@ -16,10 +16,9 @@
 #include "../include/student.h"
 using string = std::string;
 using json = nlohmann::json;
-using any = std::any;
 
 std::vector<Student> parser(const string& File, size_t len[4], json& data);
 bool input(const string& File, json& data);
-string any_print(const any& object);
+string any_print(const std::any& object);
 void print(const std::vector<Student>& students, size_t len[4]);
 #endif  // INCLUDE_PARSER_HPP_
--- include/student.h
+++ include/student.h
@@ -1,14 +1,12 @@
 #ifndef TEMPLATE_STUDENT_H
 #define TEMPLATE_STUDENT_H
 #include "parser.hpp"
-using string = std::string;
-using any = std::any;
 
 struct Student {
-  string Name;
-  any Group;
-  any Avg;
-  any Debt;
+  std::string Name;
+  std::any Group;
+  std::any Avg;
+  std::any Debt;
 };
 
 #endif  // TEMPLATE_STUDENT_H
